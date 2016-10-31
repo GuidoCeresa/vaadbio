b4e21d87255d6593318083d14f1ce6caae0da6d1
GuidoCeresa <gac@algos.it>
2016-10-31 07:20:54 +0100

    .

--- src/it/algos/vaadbio/VaadbioUI.java
+++ src/it/algos/vaadbio/VaadbioUI.java
@@ -90,7 +90,8 @@ public class VaadbioUI extends AlgosUI {
 //        footerLayout.addComponent(new Label("Vaadbio versione 4.5 del 23 apr 2016"));
 //        footerLayout.addComponent(new Label("Vaadbio versione 4.6 del 11 mag 2016"));
 //        footerLayout.addComponent(new Label("Vaadbio versione 4.7 del 27 ott 2016"));
-        footerLayout.addComponent(new Label("Vaadbio versione 4.8 del 28 ott 2016"));
+//        footerLayout.addComponent(new Label("Vaadbio versione 4.8 del 28 ott 2016"));
+        footerLayout.addComponent(new Label("Vaadbio versione 4.9 del 31 ott 2016"));
 
 //        this.checkLogin();
         boolean loggato = false;
